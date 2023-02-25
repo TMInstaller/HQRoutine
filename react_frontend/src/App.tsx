59b9ada0bb9f7e4f5e17c00067f4434130d53b2d
TMInstaller <ajvldmlqjqclr@naver.com>
2023-02-25 21:41:04 +0900

    chore : Project setting complete

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './App.css'
 
+// import pages
+import Test from './__tests__/pages/Test'
+
 // format App function to be as the typescript standard
 const App: React.FC = () => {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Test />
+    </div>
+  )
 }
 
 export default App
